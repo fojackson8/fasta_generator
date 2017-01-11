3a78ab5e934e3078b26777ac67d5da0a13ed1a8b
fojackson8 <fojackson1@gmail.com>
2017-01-11 00:04:26 +0000

    Update fasta-generator.py

--- fasta-generator.py
+++ fasta-generator.py
@@ -1,62 +1,7 @@
 
 # coding: utf-8
 
-# In[31]:
 
-test = range(1,11,1)
-import random as random
-
-from decimal import Decimal
-
-a = Decimal(0.25)
-
-type(a)
-
-
-# In[268]:
-
-# Would be good to include into this function a way of skewing the base picker with customised odds. Say you want G to
-# appear slightly less than 1 times in 4..
-
-import random 
-seq = 'chop'
-
-def random_seq(read_length,seed):
-    
-    global base
-    global seq
-    global final_seq1
-    
-    for i in range(read_length):
-        random.seed(seed)
-        choice = random.random()
-           
-        prob = 0.25
-        if choice <= prob:
-            base = 'a'
-        elif prob < choice <= 2*prob:
-            base = 'c'
-        elif 2*prob < choice <= 3*prob:
-            base = 't'
-        else:
-            base = 'g'
-        
-        seq = seq+base
-        final_seq1= seq[4:] + '\n'
-
-
-random_seq(10,1)
-final_seq1
-
-
-# In[17]:
-
-
-
-
-# In[29]:
-
-###This is the one that works!!
 import random
 
 all_chroms = []
@@ -86,73 +31,6 @@ fasta_generator(1000,20)
 
 
 
-# In[18]:
-
-all_chroms = []
-
-def fasta_generator(read_length, n_chrom):
-    global full_str
-    global all_chroms
-    global each_chrom
-    
-    for j in range(1,n_chrom+1):
-        random_seq(read_length)
-        name = '>chr%d\n' % j
-        each_chrom = {'chr_name':name, 'seq':final_seq1}
-        
-        all_chroms.append(each_chrom)
-        
-        
-        
-fasta_generator(10,4)
-      
-
-
-
-
-# In[222]:
-
-all_chroms = []
-
-def fasta_generator(read_length, n_chrom):
-    global full_str
-    global all_chroms
-    global full_fasta
-    
-    for j in range(1,n_chrom+1):
-        random_seq(read_length)
-        name = '>chr%d\n' % j
-        full_str = name +final_seq1
-        all_chroms.append(full_str)
-        full_str
-        final_seq1 = []
-        
-fasta_generator(10,4)
-        
-all_chroms
-            
-    
-
-
-# In[171]:
-
-name = 'chr%d \n' %1
-bla = 'a'
-test = name+bla
-print(test)
-
-
-# In[173]:
-
-random_base(10).final_seq
-
-
-# In[132]:
-
-my_file = open("my_fasta.txt", "w+")
-
-
-# In[ ]:
 
 
 
